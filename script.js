99735195ab0b5941feb4d5a41f5103b4044bd463
bandaraHmpl <lakshanbandara455@gmail.com>
2026-04-01 23:48:04 +0530

    Update API URL to use Railway backend

--- script.js
+++ script.js
@@ -332,7 +332,7 @@ if (skillsSection) {
 }
 
 // Load Projects from Backend
-const apiUrl = "http://my-web-backend-production.up.railway.app";
+const apiUrl = "https://my-web-backend-production.up.railway.app";
 
 function getProjectIcon(title) {
   const lowerTitle = title.toLowerCase();
